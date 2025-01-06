8597feed4d01c4a505dcf926dafab87c1656e56e
skspade <skspade@pm.me>
2025-01-05 20:50:42 -0500

    init

---
+++ scripts/player/player.gd
@@ -0,0 +1,13 @@
+# Player.gd
+extends CharacterBody2D
+
+var speed = 100
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_axis("ui_left", "ui_right")
+	input_vector.y = Input.get_axis("ui_up", "ui_down")
+	input_vector = input_vector.normalized()
+	
+	velocity = input_vector * speed
+	move_and_slide()
